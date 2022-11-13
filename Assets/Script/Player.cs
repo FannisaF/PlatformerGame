401ff8982884655560b6cc66c49cb674b75c1b44
FannisaF <fannisa.f@gmail.com>
2022-11-13 19:25:09 +0300

    First Commit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
+
+        
+    }
+}
